b932a07fcab04a1b318bd33e2a6c05686deb3965
borvux <bennyjoram@gmail.com>
2025-05-01 12:06:57 -0500

    updated solution library.rb

--- solution/library.rb
+++ solution/library.rb
@@ -6,38 +6,12 @@ class Library
   def add_book(book)
     @books.push(book)
   end
-
-  def check_out_book(title)
-    book = self.search_by_title(title)
-
-    if book.nil?
-      puts "Book not found"
-    else
-      book.check_out
-    end
-  end
-
-  def return_book(title)
-    book = self.search_by_title(title)
-
-    if book.nil?
-      puts "Book not found"
-    else
-      book.return
-    end
-  end
-
+  
   def list_items
-    puts "\n--- Library Catalog ---"
-    @books.each_with_index do |item, index|
-      puts "#{index + 1}. #{item.to_s}"
-    end
-    puts "---------------------"
+    @books
   end
-
-  private
 
   def search_by_title(title)
-    @books.find { |book| book.title == title }
+    @books.find { |book| book.title.downcase == title.downcase }
   end
 end
