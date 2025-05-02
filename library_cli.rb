c4cf17c52ded7ac08955448ec4dea51be7abb92c
borvux <bennyjoram@gmail.com>
2025-05-02 14:36:28 -0500

    updated library_cli

--- library_cli.rb
+++ library_cli.rb
@@ -11,12 +11,13 @@ class LibraryCli
 
   def run
     while true
+      system("clear")
       menu()
-      choice = get_user_input()
+      choice = get_user_input("Choose an option (1-6): ")
 
       case choice
       when "1"
-        @library.list_books
+        list_all_items()
       when "2"
         add_new_book()
       when "3"
@@ -27,6 +28,8 @@ class LibraryCli
         return_book()
       when "6"
         break
+      else
+        puts "Invalid option"
       end
 
       puts "Press Enter to continue..."
@@ -54,46 +57,57 @@ class LibraryCli
     puts "=================================="
   end
 
-  def get_user_input
-    print "Choose an option (1-6): "
+  def get_user_input(prompt)
+    print "#{prompt}"
     gets.chomp
   end
 
+  def adding(prompt)
+    puts "-- Add New #{prompt} --"
+    title = get_user_input("Enter Title: ")
+    author = get_user_input("Enter Author: ")
+    return [title, author]
+  end
+
   def add_new_book()
-    puts "-- Add New Book --"
-    print "Enter Title: "
-    title = gets.chomp
-    print "Enter Author: "
-    author = gets.chomp
+    title, author = adding("book")
     new_book = Book.new(title, author)
     @library.add_book(new_book)
+    puts "#{title} is sucessfully added"
   end
 
   def add_new_audiobook()
-    puts "-- Add New Audiobook --"
-    print "Enter Title: "
-    title = gets.chomp
-    print "Enter Author: "
-    author = gets.chomp
-    print "Enter Narrator: "
-    narrator = gets.chomp
-    print "Enter Duration (in minutes): "
-    duration = gets.chomp
+    title, author = adding("AudioBook")
+    narrator = get_user_input("Enter Narrator: ")
+    duration = get_user_input("Enter Duration (in minutes): ")
     new_audiobook = Audiobook.new(title, author, narrator, duration)
     @library.add_book(new_audiobook)
+    puts "#{title} is successfully added"
   end
 
   def checkout()
     puts "-- Check Out Item --"
-    print "Enter title to check out: "
-    title = gets.chomp
+    title = get_user_input("Enter title to check out: ")
     @library.check_out_book(title)
+    puts "#{title} is now checked out"
+  rescue => e
+    puts "Error: #{e}"
   end
 
   def return_book()
     puts "-- Return Item --"
-    print "Enter title to return: "
-    title = gets.chomp
+    title = get_user_input("Enter title to return: ")
     @library.return_book(title)
+    puts "#{title} is successfully returned"
+  rescue => e
+    puts "Error: #{e}"
+  end
+
+  def list_all_items
+    puts "--- Library Catalog ---"
+    @library.books.each_with_index do |book, index|
+      puts "#{index + 1}. #{book}"
+    end
+    puts "---------------------"
   end
 end
