eed674c1a2f8ba14d8af09b63deff966cbc0baea
borvux <bennyjoram@gmail.com>
2025-05-06 14:10:24 -0500

    moved to repo

--- solution/system.rb
+++
@@ -1,5 +0,0 @@
-require_relative "library_cli"
-
-cli = LibraryCli.new
-cli.add_sample_data
-cli.run
--- system.rb
+++
@@ -1,8 +0,0 @@
-# system.rb
-
-require_relative "library_cli"
-
-cli = LibraryCli.new
-cli.add_sample_data
-cli.run
-
